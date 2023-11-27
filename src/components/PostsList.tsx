82d9490631ee859d11eff50473188aceb3e49661
Peter Abraham <79099771+APeterIstvan@users.noreply.github.com>
2023-11-27 21:09:09 +0100

    feat(loading): handle loading state of posts

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -16,17 +16,21 @@ function PostsList({
   isPosting: boolean;
   onStopPosting: () => void;
 }) {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+
   useEffect(() => {
     async function fetchPosts() {
+      setIsFetching(true);
       const response = await fetch("http://localhost:8080/posts");
       const resData = await response.json();
       setPosts(resData.posts);
+      setIsFetching(false);
     }
 
     fetchPosts();
   }, []);
 
-  const [posts, setPosts] = useState<Post[]>([]);
   function addPostHandler(postData: Post) {
     fetch("http://localhost:8080/posts", {
       method: "POST",
@@ -48,16 +52,24 @@ function PostsList({
         </Modal>
       )}
 
-      {posts.length > 0 ? (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={Math.random()} author={post.author} body={post.body} />
           ))}
         </ul>
-      ) : (
+      )}
+
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading posts...</p>
         </div>
       )}
     </>
