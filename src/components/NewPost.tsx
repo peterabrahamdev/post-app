f0714973102d723724b2f733bfd9585a4cb44c53
Peter Abraham <79099771+APeterIstvan@users.noreply.github.com>
2023-11-27 16:25:03 +0100

    feat(new-post): add state management

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import classes from "./NewPost.module.css";
 
 function NewPost() {
+  // useState's variable always has two data:
+  // enteredBody -> current value
+  // setEnteredBody -> state updating function
+  // When you call the state updating function, React will rebuild the whole function
+  // you called it in
+  const [enteredBody, setEnteredBody] = useState("");
   function changeBodyHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
-    console.log(event.target.value);
+    setEnteredBody(event.target.value);
   }
   return (
     <form className={classes.form}>
@@ -10,6 +17,7 @@ function NewPost() {
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
+      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
