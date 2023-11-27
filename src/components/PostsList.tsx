83a6f007673afae7486b6f0bbc243c859b3f9665
Peter Abraham <79099771+APeterIstvan@users.noreply.github.com>
2023-11-27 15:59:07 +0100

    feat(new-post): create new post component

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,12 +1,16 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Peter" body="React.js is awesome" />
-      <Post author="Manuel" body="Check out my website!" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Peter" body="React.js is awesome" />
+        <Post author="Manuel" body="Check out my website!" />
+      </ul>
+    </>
   );
 }
 
