f7651113e228a56719f95f14ec9f37d8df9acf91
Peter Abraham <79099771+APeterIstvan@users.noreply.github.com>
2023-11-27 17:31:33 +0100

    feat(header): create a shared header

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { MouseEventHandler, useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 
-function PostsList() {
+function PostsList({
+  isPosting,
+  onStopPosting,
+}: {
+  isPosting: boolean;
+  onStopPosting: MouseEventHandler;
+}) {
   // useState's variable always has two data:
   // enteredBody -> current value
   // setEnteredBody -> state updating function
@@ -12,22 +18,19 @@ function PostsList() {
   // you called it in
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
-  const [modalIsVisible, setModalIsVisible] = useState(true);
+
   function bodyChangeHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
     setEnteredBody(event.target.value);
   }
   function authorChangeHandler(event: React.ChangeEvent<HTMLInputElement>) {
     setEnteredAuthor(event.target.value);
   }
-  function hideModalHandler() {
-    setModalIsVisible(false);
-  }
 
   let modalContent: JSX.Element | null = null;
 
-  if (modalIsVisible) {
+  if (isPosting) {
     modalContent = (
-      <Modal onClose={hideModalHandler}>
+      <Modal onClose={onStopPosting}>
         <NewPost
           onBodyChange={bodyChangeHandler}
           onAuthorChange={authorChangeHandler}
